4b068c25e277b84ae6a44fb09051a01960142965
luis486 <luis.murcia1@u.icesi.edu.co>
2024-04-02 16:52:51 -0500

    Container registry
    Added container registry to the infraestructure

--- main.tf
+++ main.tf
@@ -1,20 +1,9 @@
-data "azurerm_key_vault" "azure_vault" {
-  name                = var.keyvault_name
-  resource_group_name = var.keyvault_rg
-}
+# ------------------------ NETWORK -------------------------------------------
 
-data "azurerm_key_vault_secret" "ssh_public_key" {
-  name         = var.sshkvsecret
-  key_vault_id = data.azurerm_key_vault.azure_vault.id
-}
 
-data "azurerm_key_vault_secret" "spn_id" {
-  name         = var.clientidkvsecret
-  key_vault_id = data.azurerm_key_vault.azure_vault.id
-}
-data "azurerm_key_vault_secret" "spn_secret" {
-  name         = var.spnkvsecret
-  key_vault_id = data.azurerm_key_vault.azure_vault.id
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group
+  location = var.azure_region
 }
 
 resource "azurerm_virtual_network" "aks_vnet" {
@@ -32,17 +21,128 @@ resource "azurerm_subnet" "aks_subnet" {
 }
 
 
-resource "azurerm_resource_group" "aks_rg" {
-  name     = var.resource_group
-  location = var.azure_region
+# -------------------------- SECURITY----------------------------------------------
+
+# Creación del grupo de seguridad de red para permitir la comunicación a la vm
+resource "azurerm_network_security_group" "vm" {
+  name                = "vm-security-group"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "vm-ssh-rule"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "vm-access-rule"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5984"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
+
+### -----------------------CONTAINER REGISTRY--------------------- ###
+
+resource "azurerm_container_registry" "main" {
+  name                     = "myHealthContainerRegistry"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  sku                      = "Basic"
+  admin_enabled            = true
+}
+
+
+
+#------------------------------ KEY VAULT----------------------------------
+
+resource "azurerm_key_vault" "key_vault" {
+  name                        = var.keyvault_name
+  location                    = var.azure_region
+  resource_group_name         = azurerm_resource_group.aks_rg.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  enabled_for_disk_encryption = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+      "List",
+      "Create",
+      "Delete",
+    ]
+
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+    ]
+
+    storage_permissions = [
+      "get",
+      "list",
+      "delete",
+      "set",
+    ]
+  }
+
+  tags = {
+    Environment = "Production"
+    Owner       = "YourName"
+  }
+}
+
+data "azurerm_key_vault" "azure_vault" {
+  name                = var.keyvault_name
+  resource_group_name = azurerm_key_vault.key_vault.resource_group_name
+}
+
+data "azurerm_key_vault_secret" "ssh_public_key" {
+  name         = var.sshkvsecret
+  key_vault_id = azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "spn_id" {
+  name         = var.clientidkvsecret
+  key_vault_id = azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "spn_secret" {
+  name         = var.spnkvsecret
+  key_vault_id = azurerm_key_vault.key_vault.id
+}
+
+# ----------------------------- API GATEWAY ------------------------------
+
+
+# ----------------------------- CLUSTER K8S ------------------------------
+
+
+
 
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.cluster_name
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
   dns_prefix          = var.dns_name
-
 
   default_node_pool {
     name            = var.agent_pools.name
@@ -66,24 +166,4 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   tags = {
     Environment = "Demo"
   }
-
-  # If we dont use the key vault, add the next lines to apply local ssh:
-
-  #resource "tls_private_key" "linux_key" {
-  #algorithm = "RSA"
-  #rsa_bits  = 4096
-  #}
-
-# We want to save the private key to our machine
-# We can then use this key to connect to our Linux VM
-
-  #resource "local_file" "linuxkey" {
-  #filename = "linuxkey.pem"
-  #content  = tls_private_key.linux_key.private_key_pem
-  #}
-
-  #admin_ssh_key {
-  #username   = "luis486"
-  #public_key = tls_private_key.linux_key.public_key_openssh
-  #}
 }
